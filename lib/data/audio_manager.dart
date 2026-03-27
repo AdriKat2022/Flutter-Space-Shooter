cd6ed338130e33ae92d9e05474ccc367de84aa77
AdriKat <adrien.schroedel@telecom-sudparis.eu>
2026-03-27 04:04:11 +0100

    setup new version again

--- lib/data/audio_manager.dart
+++ lib/data/audio_manager.dart
@@ -1,5 +1,4 @@
 import 'package:flame/game.dart';
-import 'package:flame_audio/audio_pool.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter_space_shooter/utils/log_debug.dart';
 
